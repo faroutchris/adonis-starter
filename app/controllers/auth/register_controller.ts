6495cf178210adb0fa9a485b9edec67cd2378fb8
faroutchris <3967818+faroutchris@users.noreply.github.com>
2024-05-05 03:06:38 +0200

    Add register form and controller

---
+++ app/controllers/auth/register_controller.ts
@@ -0,0 +1,11 @@
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class RegisterController {
+  async show({ view }: HttpContext) {
+    return view.render('pages/auth/register')
+  }
+
+  async store({ response }: HttpContext) {
+    return response.redirect('index')
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -7,8 +7,9 @@
 |
 */
 
-const ArticlesController = () => import('#controllers/articles_controller')
 import router from '@adonisjs/core/services/router'
+const ArticlesController = () => import('#controllers/articles_controller')
+const RegisterController = () => import('#controllers/auth/register_controller')
 
 router.on('/').render('pages/home').as('home')
 
@@ -18,3 +19,12 @@ router
   .get('/articles/:slug', [ArticlesController, 'show'])
   .as('articles.show')
   .where('slug', router.matchers.slug())
+
+router
+  .group(() => {
+    router.get('/register', [RegisterController, 'show']).as('register.show')
+
+    router.post('/register', [RegisterController, 'store']).as('register.store')
+  })
+  .prefix('/auth')
+  .as('auth')
